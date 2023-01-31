473218d06edaa71a6a49f8a2a027005289644b98
demidraksin <greatraksin@icloud.com>
2023-01-31 17:20:56 +0300

    Adding JSON+requests

--- 20_json_requests_3101/01_json_requests.py
+++ 20_json_requests_3101/01_json_requests.py
@@ -3,9 +3,42 @@ import requests as req  # pip install requests (в терминале)
 
 def make_request(url):
     response = req.get(url)  # отсылаю GET-запрос к адресу URL
-    return response.text
+    response_json = response.json()
+    return response_json
 
 
 url = "https://jsonplaceholder.typicode.com/todos"  # сохраняем URL-адрес, по которому будем подключаться
-resp = make_request(url)
-print(resp)
+resp = make_request(url)  # здесь получаю сразу объект Python
+
+url_2 = "https://jsonplaceholder.typicode.com/users"
+resp_2 = make_request(url_2)   # получаем пользователей
+
+with open("todos.json", "w") as json_file:
+    json_file.write(json.dumps(resp, indent=4))
+
+with open("users.json", "w") as json_file:
+    json_file.write(json.dumps(resp_2, indent=4))
+
+
+count_uncompleted = 0
+for task in resp:
+    if not task["completed"]:  # если в ключе completed False
+        print(task["id"])  # вывести значение ключа id
+        count_uncompleted += 1  # увеличить счетчик
+print(f"Not completed tasks: {count_uncompleted}")
+
+users_tasks = {}
+for task in resp:
+    if task["completed"]:  # если задача имеет статус "выполнено"
+        if task["userId"] not in users_tasks:  # если пользователь с таким ID не обнаружен в словаре
+            users_tasks[task["userId"]] = 1  # добавить его ID как ключ и поставить значение выполненных задач 1
+        else:  # если пользователь с таким ID есть в словаре
+            users_tasks[task["userId"]] += 1  # добавляем этому пользователю +1 выполненную задачу
+
+print(users_tasks)
+
+users = {}
+for user in resp_2:  # перебираем юзеров
+    users[user["id"]] = user["name"]
+
+print(users)
